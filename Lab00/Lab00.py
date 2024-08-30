2ad8f6207087a2deac1598d63d4280edfe785a8e
Alessio Tucci <alessiotucci33@gmail.com>
2024-08-30 16:53:00 +0200

    working on python for now

---
+++ Lab00/Lab00.py
@@ -0,0 +1,20 @@
+# Valutare il quadrato dell'intero 9
+print(" Valutare il quadrato dell'intero 9")
+9 ** 2
+print(9 ** 2)
+
+# Valutare la divisione del numero intero per 3
+18 / 3
+
+# Restituire la concatenazione delle due parole "casa" e "bella", separate da uno spazio
+"casa" + " " + "bella"
+
+"""
+Asseganre alle variabili x e y, rispettivamente, i due numberi reali 8.1 e 9.4 e 
+restituire il valore assoluto della differenza tra x e y
+"""
+x = 8.1
+y = 9.4
+abs(x - y)
+print(abs(x - y))
+
