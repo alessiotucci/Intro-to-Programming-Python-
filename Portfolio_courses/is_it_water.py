5f65ffcb122315e5824322b6d4bc6859d7db61dc
Alessio Tucci <alessiotucci33@gmail.com>
2024-08-31 19:04:20 +0200

    fix the security reason

--- Portfolio_courses/is_it_water.py
+++ Portfolio_courses/is_it_water.py
@@ -1,6 +1,6 @@
 import requests
-api_key = "72b9bd03b8msh4831739a19d7b66p1d4855jsn16b9cdbc0c0f"
-
+# "api_key is not defined"
+api_key = "sike"
 # curl 'https://isitwater-com.p.rapidapi.com/?latitude=41.9029192&longitude=-70.2652276&rapidapi-key=YOUR-X-RAPIDAPI-KEY'
 url = "https://isitwater-com.p.rapidapi.com/"
 params = { "rapidapi-key" : api_key,
